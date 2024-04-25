7c3b1f9ffd1e2bec646e737def9478925b63a046
Collin Ballou <collin.ballou@mx.com>
2024-04-25 09:28:24 -0600

    first commit

---
+++ lua/chadrc.lua
@@ -0,0 +1,89 @@
+-- This file  needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
+
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+  theme_toggle = { "tokyonight", "solarized_dark" },
+  theme = "tokyonight", -- default theme
+  transparency = true,
+  lsp_semantic_tokens = true, -- needs nvim v0.9, just adds highlight groups for lsp semantic tokens
+
+  -- cmp themeing
+  cmp = {
+    icons = true,
+    lspkind_text = true,
+    style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+    border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
+    selected_item_bg = "colored", -- colored / simple
+  },
+
+  telescope = { style = "borderless" }, -- borderless / bordered
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+  
+  ------------------------------- nvchad_ui modules -----------------------------
+  statusline = {
+    theme = "default", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "default",
+    overriden_modules = nil,
+  },
+
+  -- lazyload it when there are 1+ buffers
+  tabufline = {
+    show_numbers = false,
+    enabled = true,
+    lazyload = true,
+    overriden_modules = nil,
+  },
+
+  -- nvdash (dashboard)
+  nvdash = {
+    load_on_startup = true,
+
+    header = {
+      "           ▄ ▄  Hello, Collin.   ",
+      "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
+      "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
+      "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
+      "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
+      "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
+      "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
+      "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
+      "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
+    },
+
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
+
+  cheatsheet = { theme = "grid" }, -- simple/grid
+
+  lsp = {
+    -- show function signatures i.e args as you type
+    signature = {
+      disabled = true,
+      silent = true, -- silences 'no signature help available' message from appearing
+    },
+  },
+}
+
+M.plugins = "" -- path i.e "custom.plugins", so make custom/plugins.lua file
+
+ -- M.lazy_nvim = require "plugins.configs.lazy_nvim" -- config for lazy.nvim startup options
+
+M.mappings = require "mappings"
+
+return M
